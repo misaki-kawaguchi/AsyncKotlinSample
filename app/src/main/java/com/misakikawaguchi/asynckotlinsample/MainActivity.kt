66b7c8725944daa0bddcc8a57a9a5789d168e653
川口美咲 <misaki@arcphilia.co.jp>
2021-02-12 21:11:07 +0900

    [Add]companion object作成

--- app/src/main/java/com/misakikawaguchi/asynckotlinsample/MainActivity.kt
+++ app/src/main/java/com/misakikawaguchi/asynckotlinsample/MainActivity.kt
@@ -4,6 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        // ログに記載するタグ用の文字列
+        private const val DEBUG_TAG =  "Async Test"
+
+        // お天気情報のURL
+        private const val WEATHERINFO_URL = "https://api.openweathermap.org/data/2.5/weather?lang=ja"
+
+        // お天気APIにアクセスするためのAPIキー
+        private const val APP_ID = ""
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
